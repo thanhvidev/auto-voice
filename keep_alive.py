b0865e447dd44a64439d69c24355b5fe696a828d
Nguyễn Hoài Thanh Vĩ <101490290+thanhvidev@users.noreply.github.com>
2024-11-08 20:51:22 +0700

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,15 +1,11 @@
+# keep_alive.py
 from flask import Flask
-from threading import Thread
 
-app = Flask('')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Bot đã hoạt động."
+    return 'Bot is running!'
 
-def run():
-  app.run(host='0.0.0.0',port=8)
-
-def keep_alive():  
-    t = Thread(target=run)
-    t.start()
+def keep_alive():
+    app.run(host='0.0.0.0', port=8080)
